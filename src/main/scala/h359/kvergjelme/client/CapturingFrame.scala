1dad411f9f5b6cef5edef548321b43dae29fbf8a
Konstantin <ostronom@gmail.com>
2013-03-18 17:45:43 +0400

    !

--- src/main/scala/h359/kvergjelme/client/CapturingFrame.scala
+++ src/main/scala/h359/kvergjelme/client/CapturingFrame.scala
@@ -1,18 +1,35 @@
 package h359.kvergjelme.client
 
 import swing._
-import java.awt.{GraphicsEnvironment, Dimension, Color, Toolkit}
-import java.awt.GraphicsDevice.WindowTranslucency._
+import swing.Swing._
+import swing.event._
+import java.awt.{Point, Dimension, Color, Toolkit}
+import javax.swing.{SwingUtilities}
 
 class CapturingFrame extends Frame {
-	val gd = GraphicsEnvironment.getLocalGraphicsEnvironment.getDefaultScreenDevice
-	if (!gd.isWindowTranslucencySupported(TRANSLUCENT)) {
-		println("Translucency not supported. Aborting.")
-		System.exit(0)
-	}
 	peer.setAlwaysOnTop(true)
 	peer.setUndecorated(true)
-	size = Toolkit.getDefaultToolkit.getScreenSize // new Dimension(640, 360)
+	//size = Toolkit.getDefaultToolkit.getScreenSize
 	background = new Color(0, 0, 0, 0.5f)
+	preferredSize  = new Dimension(640, 360)
 	visible = true
+
+	// we need panel here to gain access to mouse events
+	val virtualPanel = new Panel {
+		preferredSize  = new Dimension(640, 360)
+		background = new Color(0, 0, 0, 0.5f)
+		listenTo(mouse.clicks, mouse.moves)
+		reactions += {
+			case e: MousePressed => moveFrame(e.point)
+			case e: MouseDragged => moveFrame(e.point)
+		}
+	}
+
+	def moveFrame(p: Point) {
+		SwingUtilities.convertPointToScreen(p, virtualPanel.peer)
+		p.translate(-location.getX.intValue, -location.getY.intValue)
+		peer.setLocation(p)
+	}
+
+	contents = virtualPanel
 }
--- src/main/scala/h359/kvergjelme/client/KvergjelmeClient.scala
+++ src/main/scala/h359/kvergjelme/client/KvergjelmeClient.scala
@@ -1,9 +1,20 @@
 package h359.kvergjelme.client
 
-import swing.{SimpleSwingApplication}
-import javax.swing.{UIManager}
+import swing.{SimpleSwingApplication, Swing}
+import javax.swing.{UIManager, JFrame}
+import java.awt.{GraphicsEnvironment}
+import java.awt.GraphicsDevice.WindowTranslucency._
 
 object KvergjelmeClient extends SimpleSwingApplication {
-	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
 	val top = new KvergjelmeFrame
+
+	override def main(args: Array[String]) = Swing.onEDT {
+		val gd = GraphicsEnvironment.getLocalGraphicsEnvironment.getDefaultScreenDevice
+		if (!gd.isWindowTranslucencySupported(TRANSLUCENT)) {
+			println("Translucency not supported. Aborting.")
+			System.exit(0)
+		}
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
+		startup(args)
+	}
 }
